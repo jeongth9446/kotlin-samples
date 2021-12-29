c8cbf5ea85ad7dd6ca15f10d1986c55b22c0fa2f
JEONG TAEHUN <52663419+jeongth9446@users.noreply.github.com>
2021-12-29 16:27:57 +0900

    반복자(Iterator)
    반복자(Iterator)

---
+++ 초보자를 위한 Kotlin 200제/ex_iterator/Main.kt
@@ -0,0 +1,19 @@
+package ex_iterator
+
+fun main(args: Array<String>)
+{
+  val range: IntRange = 1..3
+  val iter: Iterator<Int> = range.iterator()
+  
+  println(iter.hasNext())
+  println(iter.next())
+  
+  println(iter.hasNext())
+  println(iter.next())
+  
+  println(iter.hasNext())
+  println(iternext())
+  
+  println(iter.hasNext())
+  
+}
