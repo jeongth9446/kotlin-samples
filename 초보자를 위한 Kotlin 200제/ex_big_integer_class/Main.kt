87f58bdc5e297899e7d236485bd894616f06de4d
JEONG TAEHUN <52663419+jeongth9446@users.noreply.github.com>
2022-01-04 16:29:32 +0900

    BigInteger 클래스 : 매우 큰 정수 보관하기
    BigInteger 클래스 : 매우 큰 정수 보관하기

---
+++ 초보자를 위한 Kotlin 200제/ex_big_integer_class/Main.kt
@@ -0,0 +1,10 @@
+package ex_big_integer_class
+
+fun main(args: Array<String>) {
+  val a = 78423343545.toBigInteger()
+  val b = 25372465331.toBigInteger()
+  
+  println(a*b)
+  println(a+b*936273.toBigInteger())
+}
+
