36afea0477a103e925e03d106a2d2a54324b4e8c
JEONG TAEHUN <52663419+jeongth9446@users.noreply.github.com>
2022-01-05 15:21:15 +0900

    지수함수
    지수함수

---
+++ 초보자를 위한 Kotlin 200제/ex_exponential_function/Main.kt
@@ -0,0 +1,10 @@
+package ex_exponential_function
+
+import kotlin.math.*
+
+fun main(args: Array<String>) {
+  println(sqrt(2.0))
+  println(2.0.pow(0.5))
+  println(exp(2.0))
+  println(expm1(2.0))
+}
