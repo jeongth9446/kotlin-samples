72eb4a1c6187829ccf3040d957c16db4d9abeec8
JEONG TAEHUN <52663419+jeongth9446@users.noreply.github.com>
2022-01-06 16:35:09 +0900

    코틀린에서 자바 코드 접근하기: 연산자 오버로딩
    코틀린에서 자바 코드 접근하기: 연산자 오버로딩

---
+++ 초보자를 위한 Kotlin 200제/ex_java_kotlin_operator_overloading/Main.kt
@@ -0,0 +1,9 @@
+package ex_java_to_kotlin_operator_overloading
+
+fun main(args: Array<String>) {
+  vals java = JavaClass()
+  println(13 in java)
+  println(java[13])
+  println(java[28])
+  println(java[18])
+}
